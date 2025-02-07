9e613684d2241ee59268cc9a3afde19cc77f82a4
James Han <hanlyu2005@gmail.com>
2025-02-06 21:57:55 -0500

    finished lab 5

--- lab5/bitmap_printer.c
+++ lab5/bitmap_printer.c
@@ -2,15 +2,21 @@
 #include <stdlib.h>
 #include "bitmap.h"
 
-
-int main(int argc, char **argv) {
-    if (argc != 2) {
+// the program name is the first argument, the file name is the second
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
         fprintf(stderr, "Usage: bitmap_printer input_bmp\n");
         exit(1);
     }
 
+    // open the file and store the file in a pointer called image
     FILE *image = fopen(argv[1], "rb");
-    if (image == NULL) {
+
+    // if the file is null, print an error message and exit the program
+    if (image == NULL)
+    {
         fprintf(stderr, "Cannot open file\n");
         exit(1);
     }
@@ -30,8 +36,10 @@ int main(int argc, char **argv) {
     fclose(image);
 
     // Print out some pixels from each of the image's corners.
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
             print_pixel(pixels[i][j]);
             print_pixel(pixels[i][width - 1 - j]);
             print_pixel(pixels[height - 1 - i][j]);
@@ -40,6 +48,12 @@ int main(int argc, char **argv) {
     }
 
     // TODO: Clean up so that valgrind reports no memory is still in use
+
+    for (int i = 0; i < height; i++)
+    {
+        free(pixels[i]);
+    }
+    free(pixels);
 
     return 0;
 }
