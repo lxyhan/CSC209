84af339f031874a2b795f662257051ccfa5d2aee
James Han <hanlyu2005@gmail.com>
2025-03-13 13:09:55 -0400

    added list_trans2

--- lab8/checkpasswd.c
+++ lab8/checkpasswd.c
@@ -17,13 +17,6 @@ int main(void)
   char user_id[MAXLINE];
   char password[MAXLINE];
 
-  /* The user will type in a user name on one line followed by a password
-     on the next.
-     DO NOT add any prompts.  The only output of this program will be one
-     of the messages defined above.
-     Please read the comments in validate carefully
-   */
-
   if (fgets(user_id, MAXLINE, stdin) == NULL)
   {
     perror("fgets");
@@ -60,6 +53,7 @@ int main(void)
   if (pid == 0)
   {
     close(fd[1]);
+
     if (dup2(fd[0], STDIN_FILENO) == -1)
     {
       perror("dup2");
@@ -67,45 +61,55 @@ int main(void)
     }
 
     close(fd[0]);
+
     execl("./validate", "validate", NULL);
 
     perror("execl");
     exit(1);
   }
-  else
+
+  close(fd[0]);
+
+  if (write(fd[1], user_id, MAX_PASSWORD) == -1)
   {
-    close(fd[0]);
-    write(fd[1], user_id, MAX_PASSWORD);
-    write(fd[1], password, MAX_PASSWORD);
-
+    perror("write");
     close(fd[1]);
+    exit(1);
+  }
 
-    int status;
-    wait(&status);
+  if (write(fd[1], password, MAX_PASSWORD) == -1)
+  {
+    perror("write");
+    close(fd[1]);
+    exit(1);
+  }
 
-    if (!WIFEXITED(status))
-    {
-      exit(1);
-    }
+  close(fd[1]);
+  int status;
+  wait(&status);
 
-    int exit_status = WEXITSTATUS(status);
+  if (!WIFEXITED(status))
+  {
+    exit(1);
+  }
 
-    if (exit_status == 0)
-    {
-      printf(SUCCESS);
-    }
-    else if (exit_status == 1)
-    {
-      exit(0);
-    }
-    else if (exit_status == 2)
-    {
-      printf(INVALID);
-    }
-    else if (exit_status == 3)
-    {
-      printf(NO_USER);
-    }
+  int exit_status = WEXITSTATUS(status);
+
+  if (exit_status == 0)
+  {
+    printf(SUCCESS);
+  }
+  else if (exit_status == 1)
+  {
+    exit(0);
+  }
+  else if (exit_status == 2)
+  {
+    printf(INVALID);
+  }
+  else if (exit_status == 3)
+  {
+    printf(NO_USER);
   }
 
   return 0;
